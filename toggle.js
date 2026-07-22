472045a5f4bec2ac93ebbd2245afb489035c58cb
Kane2881 <kanethepro2881@gmail.com>
2026-07-22 23:12:08 +0900

    finished code

--- toggle.js
+++ toggle.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const socialGroup = document.getElementById("social-links-group");
+// const square = document.querySelector(".square");
+const socialLinkWrapper = document.querySelector(".social-links-wrapper");
 
 toggleBtn.addEventListener("click", () => {
   const isExpanded = toggleBtn.getAttribute("aria-expanded") === "true";
@@ -9,9 +11,13 @@ toggleBtn.addEventListener("click", () => {
 function openSlideIn() {
   toggleBtn.setAttribute("aria-expanded", true);
   socialGroup.setAttribute("aria-hidden", false);
+  socialLinkWrapper.style.visibility = "visible";
+  socialLinkWrapper.style.opacity = "1";
 }
 
 function closeSlideBack() {
   toggleBtn.setAttribute("aria-expanded", false);
   socialGroup.setAttribute("aria-hidden", true);
+  socialLinkWrapper.style.visibility = "hidden";
+  socialLinkWrapper.style.opacity = "0";
 }
